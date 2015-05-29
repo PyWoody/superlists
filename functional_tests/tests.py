c5e7205a4ad92a43ae8a1152a397eb53db46dd0d
Sam Woodward <samuel.j.woodward@gmail.com>
2015-05-29 17:29:11 -0400

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -13,6 +13,21 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def test_layout_and_styling(self):
+		# Edith goes to the home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		# She starts a new list and sees the input is nicely
+		# centered there too
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
+
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
@@ -96,3 +111,4 @@ class NewVisitorTest(LiveServerTestCase):
 		# # There is still a text box inviting her to add another item. She
 		# # enters "Use peacock feathers to make a fly"
 		# self.fail('Finish the test!')
+
