997c39f9523750042f6c3b4fa868678d9b113f3d
Sam Woodward <samuel.j.woodward@gmail.com>
2015-07-06 19:16:31 -0400

    Refactor hard-coded URLs out of templates

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -32,8 +32,6 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
 
-
-
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
         item = Item(list=list_, text='')
